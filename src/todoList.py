28c29dd7894616c9b32129c2d57a7aa16c02646d
Ubuntu <ubuntu@ip-172-31-86-170.ec2.internal>
2026-05-24 02:08:18 +0000

    Add static Test stage - Flake8 y Bandit

--- src/todoList.py
+++ src/todoList.py
@@ -16,6 +16,9 @@ def get_table(dynamodb=None):
             boto3.resource = functools.partial(boto3.resource,
                                                endpoint_url=URL)
         dynamodb = boto3.resource("dynamodb")
+        db_password = "clave_secreta_123"
+        os.system("ls " + os.environ['DYNAMODB_TABLE'])
+        eval("print('test')")
     # fetch todo from the database
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
     return table
